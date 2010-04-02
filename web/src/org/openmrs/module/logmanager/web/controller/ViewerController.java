a5f87dc88bb150d97798d62302152d1a8d3f8f35
Rowan Seymour <rowanseymour@gmail.com>
2010-04-02 13:39:29 +0000

    logmanager: Added option for check/recreate for system appender to configuration page

--- web/src/org/openmrs/module/logmanager/web/controller/ViewerController.java
+++ web/src/org/openmrs/module/logmanager/web/controller/ViewerController.java
@@ -28,6 +28,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.spi.LoggingEvent;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.logmanager.AppenderProxy;
+import org.openmrs.module.logmanager.Config;
 import org.openmrs.module.logmanager.Constants;
 import org.openmrs.module.logmanager.LogManagerService;
 import org.openmrs.module.logmanager.QueryField;
@@ -58,8 +59,9 @@ public class ViewerController extends ParameterizableViewController {
 		
 		// Ensure that the memory appender defined in OpenMRS's log4j.xml exists
 		// and configure it to be used as the system appender
-		if (!LogManagerUtils.ensureSystemAppenderExists())
-			WebUtils.setErrorMessage(request, Constants.MODULE_ID + ".viewer.systemAppenderRecreatedMsg", null);
+		if (Config.getInstance().isAlwaysRecreateSystemAppender())
+			if (!LogManagerUtils.ensureSystemAppenderExists())
+				WebUtils.setErrorMessage(request, Constants.MODULE_ID + ".viewer.systemAppenderRecreatedMsg", null);
 		
 		Map<String, Object> model = new HashMap<String, Object>();
 		LogManagerService svc = Context.getService(LogManagerService.class);
